1b187c92852f7c8675e734a6acd8dfd03c575205
Phil BStorm <philippe.haerens@bstorm.be>
2025-11-26 13:57:11 +0100

    Utilisation de ViewData

---
+++ DemoMVC/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Objet "ViewData" permet de passer des données du contrôleur à la vue
+            ViewData["date"] = DateTime.Today;
+            ViewData["nb"] = 42;
+
+            // Génération de la vue
+            return View();
+        }
+    }
+}
